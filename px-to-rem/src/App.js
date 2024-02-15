51287d1816d31a4db02024a9c96ef4b82ae624e5
Karan Saket <karansaket1995@gmail.com>
2024-02-15 12:07:50 +0530

    created px to rem converter

---
+++ px-to-rem/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PxToRemConverter from './PxToRemConverter/PxToRemConverter';
+
+function App() {
+  return (
+    <div className="App">
+    <PxToRemConverter/>
+    </div>
+  );
+}
+
+export default App;
